84835121ac1961d13567266e3a4a0be18fecaaa3
circuitdh <circuitdh@gmail.com>
2016-07-09 19:34:22 -0700

    Update ClientProxy.java

--- src - Copy/main/java/net/bleachcraft/Proxy/ClientProxy.java
+++ src - Copy/main/java/net/bleachcraft/Proxy/ClientProxy.java
@@ -11,40 +11,35 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class ClientProxy extends CommonProxy
-{
-     private static ModelResourceLocation gasLocation = new ModelResourceLocation("bleachcraft" + ":" + "ChlorineBlock", "gas");
+public class ClientProxy extends CommonProxy {
+	private static ModelResourceLocation gasLocation = new ModelResourceLocation("bleachcraft" + ":" + "ChlorineBlock",
+			"gas");
 
-     @Override
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        super.preInit(event);
-        Item gas = Item.getItemFromBlock(ChlorineBlock.instance);
-        ModelBakery.registerItemVariants(gas);
-        ModelLoader.setCustomMeshDefinition(gas, new ItemMeshDefinition()
-        {
-            public ModelResourceLocation getModelLocation(ItemStack stack)
-            {
-                return gasLocation;
-            }
-        });
-        ModelLoader.setCustomStateMapper(ChlorineBlock.instance, new StateMapperBase()
-        {
-            protected ModelResourceLocation getModelResourceLocation(IBlockState state)
-            {
-                return gasLocation;
-            }
-        });
-    }
-    
-    public static void registerItemRender(Item item, int meta)
-    {
-    	ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), "inventory"));
-    }
-    
-    public static void registerFluidRender(Item item, int meta, String location, String varient)
-    {
-    	ModelLoader.setCustomModelResourceLocation(item, meta,new ModelResourceLocation(location, varient));
-    }
-    
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		super.preInit(event);
+		Item gas = Item.getItemFromBlock(ChlorineBlock.instance);
+		ModelBakery.registerItemVariants(gas);
+		ModelLoader.setCustomMeshDefinition(gas, new ItemMeshDefinition() {
+			public ModelResourceLocation getModelLocation(ItemStack stack) {
+				return gasLocation;
+			}
+		});
+		ModelLoader.setCustomStateMapper(ChlorineBlock.instance, new StateMapperBase() {
+			protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+				return gasLocation;
+			}
+		});
+
+	}
+
+	public static void registerItemRender(Item item, int meta) {
+		ModelLoader.setCustomModelResourceLocation(item, meta,
+				new ModelResourceLocation(item.getRegistryName(), "inventory"));
+	}
+
+	public static void registerFluidRender(Item item, int meta, String location, String varient) {
+		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(location, varient));
+	}
+
 }
