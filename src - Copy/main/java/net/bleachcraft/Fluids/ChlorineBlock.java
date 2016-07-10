348f7dbff56b5394d11f2b50220dafe5ba2f9059
circuitdh <circuitdh@gmail.com>
2016-07-09 19:29:43 -0700

    Update ChlorineBlock.java

--- src - Copy/main/java/net/bleachcraft/Fluids/ChlorineBlock.java
+++ src - Copy/main/java/net/bleachcraft/Fluids/ChlorineBlock.java
@@ -6,16 +6,14 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fluids.BlockFluidClassic;
 
-public class ChlorineBlock extends BlockFluidClassic 
-{
+public class ChlorineBlock extends BlockFluidClassic {
 	public static final ChlorineBlock instance = new ChlorineBlock();
-    
-    public ChlorineBlock()
-    {
-        super(Chlorine.instance, Material.WATER);
-        setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
-        setUnlocalizedName("bleachcraft" + ":" + "ChlorineBlock");
 
-        setRegistryName("ChlorineBlock");
-    }
+	public ChlorineBlock() {
+		super(Chlorine.instance, Material.WATER);
+		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		setUnlocalizedName("bleachcraft" + ":" + "ChlorineBlock");
+
+		setRegistryName("ChlorineBlock");
+	}
 }
