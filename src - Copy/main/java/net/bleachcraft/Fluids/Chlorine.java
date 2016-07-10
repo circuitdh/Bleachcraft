fa20e56d76aadd4df634c0a7fbe22a6c2247283b
circuitdh <circuitdh@gmail.com>
2016-07-09 19:29:19 -0700

    Update Chlorine.java

--- src - Copy/main/java/net/bleachcraft/Fluids/Chlorine.java
+++ src - Copy/main/java/net/bleachcraft/Fluids/Chlorine.java
@@ -3,16 +3,15 @@ package net.bleachcraft.Fluids;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
 
-public class Chlorine extends Fluid
-{
+public class Chlorine extends Fluid {
 	public static final String name = "chlorine";
-    public static final Chlorine instance = new Chlorine();
+	public static final Chlorine instance = new Chlorine();
 
-    public Chlorine()
-    {
-        super(name, new ResourceLocation("bleachcraft:blocks/chlorine_still"), new ResourceLocation("bleachcraft:blocks/chlorine_flow"));
-        density = -1000;
-        isGaseous = true;
-    }
+	public Chlorine() {
+		super(name, new ResourceLocation("bleachcraft:blocks/chlorine_still"),
+				new ResourceLocation("bleachcraft:blocks/chlorine_flow"));
+		density = -1000;
+		isGaseous = true;
+	}
 
 }
