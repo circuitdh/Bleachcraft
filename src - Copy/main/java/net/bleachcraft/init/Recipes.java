f4ec2097114dbf70c10b813839f9d2bcb3f8ef93
circuitdh <circuitdh@gmail.com>
2016-07-09 19:31:42 -0700

    Update Recipes.java

--- src - Copy/main/java/net/bleachcraft/init/Recipes.java
+++ src - Copy/main/java/net/bleachcraft/init/Recipes.java
@@ -2,15 +2,18 @@ package net.bleachcraft.init;
 
 import net.bleachcraft.Items.BleachBottle;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class Recipes {
-	
-	                        
 	public static void registerRecipes() {
 		//Bleach Bottle Recipe
 		GameRegistry.addRecipe(new ItemStack(net.bleachcraft.init.ModItems.bleachBottle), "brb", "bmb", "bbb",'b',new ItemStack(Blocks.STAINED_GLASS,1,11), 'r', new ItemStack(Blocks.STAINED_GLASS,1,14),'m',net.bleachcraft.init.ModItems.bleachChemicals);
+		//Noodle recipe
+		
+		//Chicken Noodle Soup recipe
+		GameRegistry.addShapelessRecipe(new ItemStack(net.bleachcraft.init.ModItems.chickenNoodleSoup), new ItemStack(Items.BOWL), new ItemStack(Items.CHICKEN), new ItemStack(net.bleachcraft.init.ModItems.noodle), new ItemStack(net.bleachcraft.init.ModItems.bleachBottle));
 	}
-
+	
 }
