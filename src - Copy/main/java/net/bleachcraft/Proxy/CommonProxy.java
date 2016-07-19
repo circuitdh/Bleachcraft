b62ea0c4c1a0ac8d0725d5b00ee1f6065f8f8b18
circuitdh <circuitdh@gmail.com>
2016-07-19 12:24:18 -0700

    Update CommonProxy.java

--- src - Copy/main/java/net/bleachcraft/Proxy/CommonProxy.java
+++ src - Copy/main/java/net/bleachcraft/Proxy/CommonProxy.java
@@ -2,13 +2,13 @@ package net.bleachcraft.Proxy;
 
 import net.bleachcraft.Fluids.Chlorine;
 import net.bleachcraft.Fluids.ChlorineBlock;
+import net.bleachcraft.init.Fluids;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
 	public void preInit(FMLPreInitializationEvent event) {
-
+		FluidRegistry.registerFluid(Chlorine.instance);
 	}
 
 	public void registerRenderThingsInit() {
